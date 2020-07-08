9d37ff376cc80219aa7353117a35960ded65a1b7
Piotr <piotr@pop-os.localdomain>
2020-07-08 16:42:06 +0200

    add configuration file with clientid and secret for facebook and github. Google is having some issues so add this later. git status

--- spring-oauth2/src/main/java/com/example/springoauth2/SpringOauth2Application.java
+++ spring-oauth2/src/main/java/com/example/springoauth2/SpringOauth2Application.java
@@ -1,9 +1,12 @@
 package com.example.springoauth2;
 
+import com.example.springoauth2.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class SpringOauth2Application {
 
 	public static void main(String[] args) {
---
+++ spring-oauth2/src/main/java/com/example/springoauth2/config/AppProperties.java
@@ -0,0 +1,7 @@
+package com.example.springoauth2.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+}
