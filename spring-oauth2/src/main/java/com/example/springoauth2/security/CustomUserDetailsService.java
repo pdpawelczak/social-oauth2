d32eb906e17c97cd31894d76e8e22f92d6f71c60
Piotr <piotr@pop-os.localdomain>
2020-07-09 16:47:56 +0200

    CustomUserDetailsService which has a method loadByUsername that allows to load user details. UserPrincipal contains information about currently logged user.

---
+++ spring-oauth2/src/main/java/com/example/springoauth2/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.springoauth2.security;
+
+import com.example.springoauth2.model.User;
+import com.example.springoauth2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + email +
+                        "not found"));
+        return UserPrincipal.create(user);
+    }
+
+
+}
